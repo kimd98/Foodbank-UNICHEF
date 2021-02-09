c2ab52d0f4a995a529086a300b92c95ad75153d5
Dayoung Kim <lenakim0215@gmail.com>
2021-02-08 23:21:44 -0800

    combined login landing page

--- app/src/main/java/com/example/foodbank2021/MainActivity.java
+++ app/src/main/java/com/example/foodbank2021/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.foodbank2021;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -27,157 +26,148 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText username,
-            password;
     private Button login;
     private TextView register;
-    private EditText editTextEmail, editTextPassword;
-    private Button signIn;
-    private FirebaseAuth mAuth;
+    private EditText user_email, user_password;
     private ProgressBar progressBar;
+    Switch active; // for remember me
 
-    Switch active;
-
-
+    // This is a landing login page
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        register=(TextView) findViewById(R.id.signup);
-        register.setOnClickListener(this);
-        signIn=(Button)findViewById(R.id.login);
-        signIn.setOnClickListener(this);
-        editTextEmail=(EditText)findViewById(R.id.email);
-        editTextPassword=(EditText)findViewById(R.id.password);
 
-        username = findViewById(R.id.email);
-        password = findViewById(R.id.password);
+        // replaced signIn button with login & username/password removed
+        register=(TextView)findViewById(R.id.signup);
         login = findViewById(R.id.login);
+        user_email=(EditText)findViewById(R.id.email);
+        user_password=(EditText)findViewById(R.id.password);
         active = findViewById(R.id.active);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-                databaseReference.child("login").addValueEventListener(new ValueEventListener() {
+        // links to onClick function
+        register.setOnClickListener(this);
+        login.setOnClickListener(this);
+    };
+
+    // move to sign-up (Registration) or sign-in (Login) page
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+            case R.id.login:
+                userLogin();
+                break;
+        }
+    };
+
+    // a helper function for login info validity check
+    private boolean validityCheck(String input_email, String input_password) {
+        if (input_email.isEmpty()) {
+            user_email.setError("Email is required.");
+            user_email.requestFocus();
+            return true;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(input_email).matches()) {
+            user_email.setError("Please enter valid email.");
+            user_email.requestFocus();
+            return true;
+        }
+        if (input_password.isEmpty()) {
+            user_password.setError("Password is required.");
+            user_password.requestFocus();
+            return true;
+        } else if (input_password.length() < 6) {
+            user_password.setError("Minimum password length should be 6 characters.");
+            user_password.requestFocus();
+            return true;
+        }
+        return false;
+    };
+
+    // login activity (validity check, account match, google authentication)
+    private void userLogin() {
+        // casting global user data to string
+        String email = user_email.getText().toString().trim();
+        String password = user_password.getText().toString().trim();
+
+        // I don't know what this is :) ?
+        progressBar.setVisibility(View.VISIBLE);
+
+        // if input data is invalid, do nothing
+        if (validityCheck(email, password))
+            return;
+
+        // Firebase Authentication Sign-in Method (reference to "Users")
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+            // a helper function to login as admin/user
+            private void login(String uid) {
+                DatabaseReference userRef = databaseReference.child(uid);
+                userRef.addValueEventListener(new ValueEventListener() {    // attach a listener to get "as" value
                     @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        String input1 = username.getText().toString();
-                        String input2 = password.getText().toString();
-
-                        if (dataSnapshot.child(input1).exists()) {
-                            if (dataSnapshot.child(input1).child("password").getValue(String.class).equals(input2)) {
-                                if (active.isChecked()) { // remember me
-                                    if (dataSnapshot.child(input1).child("as").getValue(String.class).equals("admin")) { // remember admin
-                                        preferences.setDataLogin(MainActivity.this, true);
-                                        preferences.setDataAs(MainActivity.this, "admin");
-                                        startActivity(new Intent(MainActivity.this, AdminActivity.class));
-                                    } else if (dataSnapshot.child(input1).child("as").getValue(String.class).equals("user")){ // remember user
-                                        preferences.setDataLogin(MainActivity.this, true);
-                                        preferences.setDataAs(MainActivity.this, "user");
-                                        startActivity(new Intent(MainActivity.this, UserActivity.class));
-                                    }
-                                } else {
-                                    if (dataSnapshot.child(input1).child("as").getValue(String.class).equals("admin")) {
-                                        preferences.setDataLogin(MainActivity.this, false);
-                                        startActivity(new Intent(MainActivity.this, AdminActivity.class));
-                                    } else if (dataSnapshot.child(input1).child("as").getValue(String.class).equals("user")){
-                                        preferences.setDataLogin(MainActivity.this, false);
-                                        startActivity(new Intent(MainActivity.this, UserActivity.class));
-                                    }
-                                }
-                            } else {
-                                Toast.makeText(MainActivity.this, "password", Toast.LENGTH_SHORT).show();
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (active.isChecked()) {   // remember me switch ON ("as" will be saved)
+                            if (snapshot.child("as").getValue(String.class).equals("admin")) { // admin
+                                preferences.setDataLogin(MainActivity.this, true);
+                                preferences.setDataAs(MainActivity.this, "admin");
+                                startActivity(new Intent(MainActivity.this, AdminActivity.class));
+                            } else if (snapshot.child("as").getValue(String.class).equals("user")) { // user
+                                preferences.setDataLogin(MainActivity.this, true);
+                                preferences.setDataAs(MainActivity.this, "user");
+                                startActivity(new Intent(MainActivity.this, UserActivity.class));
                             }
-                        } else {
-                            Toast.makeText(MainActivity.this, "username", Toast.LENGTH_SHORT).show();
+                        } else {    // remember me switch OFF ("as" history will be ignored)
+                            if (snapshot.child("as").getValue(String.class).equals("admin")) { // admin
+                                preferences.setDataLogin(MainActivity.this, false);
+                                startActivity(new Intent(MainActivity.this, AdminActivity.class));
+                            } else if (snapshot.child("as").getValue(String.class).equals("user")) { // user
+                                preferences.setDataLogin(MainActivity.this, false);
+                                startActivity(new Intent(MainActivity.this, UserActivity.class));
+                            }
                         }
-                    }
-
+                    };
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
+                        // do nothing
+                    };
                 });
+            };
+
+            // core authentication process using the previous helper function
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+                    if (user.isEmailVerified()) {
+                        login(user.getUid());
+                    } else {    // email not verified
+                        user.sendEmailVerification();
+                        Toast.makeText(MainActivity.this,"Check your email to verify your account.",Toast.LENGTH_LONG).show();
+                    }
+                } else {    // unsuccessful task
+                    Toast.makeText(MainActivity.this,"Failed to login! Please check your credentials.",Toast.LENGTH_LONG).show();
+                }
             };
         });
     };
 
-    // function to remember the account using an active switch
+    // choose either admin page or user page after successfully log into the app
     @Override
     protected void onStart() {
         super.onStart();
         if (preferences.getDataLogin(this)) {
-           if (preferences.getDataAs(this).equals("admin")) {
-               startActivity(new Intent(MainActivity.this, AdminActivity.class));
-               finish();
-           } else if (preferences.getDataAs(this).equals("user")) {
-               startActivity(new Intent(MainActivity.this, UserActivity.class));
-               finish();
-           }
+            if (preferences.getDataAs(this).equals("admin")) {
+                startActivity(new Intent(MainActivity.this, AdminActivity.class));
+                finish();
+            } else if (preferences.getDataAs(this).equals("user")) {
+                startActivity(new Intent(MainActivity.this, UserActivity.class));
+                finish();
+            }
         }
     };
-
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.register:
-                startActivity(new Intent(this, RegisterUser.class));
-                break;
-
-            case R.id.signIn:
-                userLogin();
-                break;
-
-        }
-    }
-
-    private void userLogin() {
-        String email=editTextEmail.getText().toString().trim();
-        String password=editTextPassword.getText().toString().trim();
-
-        if(email.isEmpty()){
-            editTextEmail.setError("Email is required!");
-            editTextEmail.requestFocus();
-            return;
-        }
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            editTextEmail.setError("Please enter valid email!");
-            editTextEmail.requestFocus();
-            return;
-        }
-        if(password.isEmpty()){
-            editTextPassword.setError("Password is required!");
-            editTextPassword.requestFocus();
-            return;
-        }
-        if(password.length()<6){
-            editTextPassword.setError("Min password length should be 6 characters!");
-            editTextPassword.requestFocus();
-            return;
-        }
-
-        progressBar.setVisibility(View.VISIBLE);
-
-        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                if(task.isSuccessful()){
-                    FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
-
-                    if(user.isEmailVerified()){
-                        startActivity(new Intent(MainActivity.this,UserActivity.class));
-                    }else{
-                        user.sendEmailVerification();
-                        Toast.makeText(MainActivity.this,"Check your email to verifiy your account!",Toast.LENGTH_LONG).show();
-                    }
-                    startActivity(new Intent(MainActivity.this,UserActivity.class));
-
-                }else{
-                    Toast.makeText(MainActivity.this,"Failed to login! Please check your credentials",Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-    }
 }
--- app/src/main/java/com/example/foodbank2021/User.java
+++ app/src/main/java/com/example/foodbank2021/User.java
@@ -2,13 +2,12 @@ package com.example.foodbank2021;
 
 public class User {
 
-    public String firstName,lastName,email;
-    public User(){
+    public String firstName, lastName, email, as;
 
-    }
-    public User(String firstName,String lastName,String email){
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.email=email;
+    public User(String firstName, String lastName, String email, String as) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.as = as;
     }
 }
