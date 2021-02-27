3a13b8701990fe3905d5a2b1f7c933fb0fbe0d8c
jiin9802 <jiin9802@naver.com>
2021-02-28 04:44:04 +0900

    qrcode

--- app/src/main/java/com/example/foodbank2021/createQRcode.java
+++ app/src/main/java/com/example/foodbank2021/createQRcode.java
@@ -2,13 +2,32 @@ package com.example.foodbank2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class createQRcode extends AppCompatActivity {
-
+    private ImageView iv;
+    private String text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_q_rcode);
+        iv = (ImageView)findViewById(R.id.qrcode);
+        text = "https://park-duck.tistory.com";
+
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try{
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            iv.setImageBitmap(bitmap);
+        }catch (Exception e){}
     }
 }
