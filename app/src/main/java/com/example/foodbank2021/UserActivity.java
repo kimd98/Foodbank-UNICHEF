3e9958bc05342c0956733fb8ff7b48277206fa4f
jiin9802 <jiin9802@naver.com>
2021-03-03 05:47:26 +0900

    sidebar

--- app/src/main/java/com/example/foodbank2021/UserActivity.java
+++ app/src/main/java/com/example/foodbank2021/UserActivity.java
@@ -3,14 +3,17 @@ package com.example.foodbank2021;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
@@ -31,6 +34,9 @@ import com.google.firebase.database.ValueEventListener;
 
 public class UserActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
     private AppBarConfiguration mAppBarConfiguration;
     private MenuItem item;
     Toolbar toolbar;
@@ -43,7 +49,10 @@ public class UserActivity extends AppCompatActivity {
 
         // fragment title tool bar
         toolbar = findViewById(R.id.toolbar);
-
+        //sidebar
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.nav_view);
+        setSupportActionBar(toolbar);
         // instantiated landing page
         if (savedInstanceState == null) {
             toolbar.setTitle("Home");
@@ -57,61 +66,92 @@ public class UserActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         BottomNavigationView navView = findViewById(R.id.bottom_nav);
         navView.setOnNavigationItemSelectedListener(navListener);
-
-        /*
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.side_nav);
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_profile, R.id.nav_qrcode)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-         */
+        /*---------------side bar----------------*/
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(sidenavListener);
     }
-
-    // bottom action bar landing fragments
-    private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @SuppressLint("NonConstantResourceId")
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+    private final NavigationView.OnNavigationItemSelectedListener sidenavListener=
+            new NavigationView.OnNavigationItemSelectedListener() {
                 @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = new HomeFragment();
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_home:
+                            startActivity(new Intent(UserActivity.this, UserActivity.class));
+                            break;
+                        case R.id.nav_profile:
+                            startActivity(new Intent(UserActivity.this, ProfileActivity.class));
+                            break;
+                        case R.id.nav_qrcode:
+                            if (checkVerified(user_uid)) {
+                                startActivity(new Intent(UserActivity.this, createQRcode.class));
+                                break;
+                            } else {
+                                Toast.makeText(UserActivity.this, "Please verify your account first!", Toast.LENGTH_LONG).show();
+                            }
+                        case R.id.nav_logout:
+                            startActivity(new Intent(UserActivity.this, MainActivity.class));
+                            break;
 
-                    switch (item.getItemId()) {
-                        case R.id.navigation_home:
-                            selectedFragment = new HomeFragment();
-                            toolbar.setTitle("Home");
-                            break;
-                        case R.id.navigation_map:
-                            selectedFragment = new MapFragment();
-                            toolbar.setTitle("Map");
-                            break;
-                        case R.id.navigation_notifications:
-                            selectedFragment = new NotificationsFragment();
-                            toolbar.setTitle("Notifications");
-                            break;
-                        case R.id.navigation_messenger:
-                            selectedFragment = new MessengerFragment();
-                            toolbar.setTitle("Messenger");
-                            break;
-                        default:
-                            throw new IllegalStateException("Unexpected value: " + item.getItemId());
                     }
-
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
+                    drawerLayout.closeDrawer(GravityCompat.START);
                     return true;
                 }
             };
+                // bottom action bar landing fragments
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
+                        new BottomNavigationView.OnNavigationItemSelectedListener() {
+                            @SuppressLint("NonConstantResourceId")
+                            @Override
+                            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                                Fragment selectedFragment = new HomeFragment();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.user, menu);
-        return true;
-    }
+                                switch (item.getItemId()) {
+                                    case R.id.navigation_home:
+                                        selectedFragment = new HomeFragment();
+                                        toolbar.setTitle("Home");
+                                        break;
+                                    case R.id.navigation_map:
+                                        selectedFragment = new MapFragment();
+                                        toolbar.setTitle("Map");
+                                        break;
+                                    case R.id.navigation_notifications:
+                                        selectedFragment = new NotificationsFragment();
+                                        toolbar.setTitle("Notifications");
+                                        break;
+                                    case R.id.navigation_messenger:
+                                        selectedFragment = new MessengerFragment();
+                                        toolbar.setTitle("Messenger");
+                                        break;
+                                    default:
+                                        throw new IllegalStateException("Unexpected value: " + item.getItemId());
+                                }
+
+                                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                        selectedFragment).commit();
+                                return true;
+
+                            }
+                        };
+
+
+
+                            @Override
+                            public boolean onCreateOptionsMenu(Menu menu) {
+                                // Inflate the menu; this adds items to the action bar if it is present.
+                                getMenuInflater().inflate(R.menu.user, menu);
+                                return true;
+                            }
 
     /*
     @Override
@@ -122,36 +162,27 @@ public class UserActivity extends AppCompatActivity {
     }
      */
 
-    public void logout(MenuItem item) {
-        startActivity(new Intent(UserActivity.this, MainActivity.class));
-        preferences.clearData(this);
-        finish();
-    }
-    public void qrcode(MenuItem item){
-        if (checkVerified(user_uid)) {
-            startActivity(new Intent(UserActivity.this, createQRcode.class));
-        }
-    }
 
-    public boolean checkVerified (String checkUid) {
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users");
-        DatabaseReference userRef = databaseReference.child(user_uid);
-        final boolean[] verified = {true};
 
-        ValueEventListener valueEventListener = userRef.addValueEventListener(new ValueEventListener() {    // attach a listener to get "as" value
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.child("verified").getValue(String.class).equals("no")) {
-                    Toast.makeText(UserActivity.this, "Please verify your account.", Toast.LENGTH_LONG).show();
-                    verified[0] = false;
-                }
-            }
+                            public boolean checkVerified(String checkUid) {
+                                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+                                DatabaseReference userRef = databaseReference.child(user_uid);
+                                final boolean[] verified = {true};
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                // do nothing
-            }
-        });
-        return verified[0];
-    }
-}
+                                ValueEventListener valueEventListener = userRef.addValueEventListener(new ValueEventListener() {    // attach a listener to get "as" value
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        if (snapshot.child("verified").getValue(String.class).equals("no")) {
+                                            Toast.makeText(UserActivity.this, "Please verify your account.", Toast.LENGTH_LONG).show();
+                                            verified[0] = false;
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+                                        // do nothing
+                                    }
+                                });
+                                return verified[0];
+                            }
+                        }
