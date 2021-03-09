5f16df4e113f3af441c7a4e2b6c35aadbedde58c
jiin9802 <jiin9802@naver.com>
2021-03-10 04:30:44 +0900

    admin

--- app/src/main/java/com/example/foodbank2021/AdminActivity.java
+++ app/src/main/java/com/example/foodbank2021/AdminActivity.java
@@ -1,20 +1,33 @@
 package com.example.foodbank2021;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.ui.AppBarConfiguration;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private AppBarConfiguration mAppBarConfiguration;
+    private MenuItem item;
+    private final String user_uid= FirebaseAuth.getInstance().getCurrentUser().getUid();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,14 @@ public class AdminActivity extends AppCompatActivity {
 
         // fragment title tool bar
         toolbar = findViewById(R.id.toolbar);
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.nav_view);
+        setSupportActionBar(toolbar);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(sidenavListener);
 
         // instantiated landing page
         if (savedInstanceState == null) {
@@ -39,7 +60,34 @@ public class AdminActivity extends AppCompatActivity {
         navView.setOnNavigationItemSelectedListener(navListener);
     }
 
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
+    private final NavigationView.OnNavigationItemSelectedListener sidenavListener=
+            new NavigationView.OnNavigationItemSelectedListener() {
+
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+//                            startActivity(new Intent(UserActivity.this, UserActivity.class));
+                            break;
+                        case R.id.nav_ams:
+                            startActivity(new Intent(AdminActivity.this, MemberActivity.class));
+                            break;
+                        case R.id.nav_logout:
+                            startActivity(new Intent(AdminActivity.this, MainActivity.class));
+                            break;
+                    }
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+            };
     // bottom action bar landing fragments
     private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
---
+++ app/src/main/java/com/example/foodbank2021/MemberActivity.java
@@ -0,0 +1,14 @@
+package com.example.foodbank2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member);
+    }
+}
